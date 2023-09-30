aaf2b8d1041b510f34d71abeb91f57f297cb35de
Mohamed Amine Ben Salem <abdbsalem@gmail.com>
2023-09-30 17:11:11 +0100

    jwt converter

--- src/main/java/com/m/keycloak/DemoController.java
+++ src/main/java/com/m/keycloak/DemoController.java
@@ -1,5 +1,6 @@
 package com.m.keycloak;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,11 +9,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/vi/demo")
 public class DemoController {
     @GetMapping
+    @PreAuthorize("hasRole('user')")
     public String hello() {
         return "Hello from Spring boot & Keycloak";
     }
 
     @GetMapping("/helloAdmin")
+    @PreAuthorize("hasRole('admin')")
     public String helloAdmin() {
         return "Hello from Spring boot & Keycloak - ADMIN";
     }
--- src/main/java/com/m/keycloak/SecurityConfig.java
+++ src/main/java/com/m/keycloak/SecurityConfig.java
@@ -1,7 +1,9 @@
 package com.m.keycloak;
 
+import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -9,12 +11,16 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
+@AllArgsConstructor
 public class SecurityConfig {
+
+    private JwtAuthConverter jwtAuthConverter;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf().disable().authorizeHttpRequests().anyRequest().authenticated();
-        http.oauth2ResourceServer().jwt();
+        http.oauth2ResourceServer().jwt().jwtAuthenticationConverter(jwtAuthConverter);
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         return http.build();
     }
